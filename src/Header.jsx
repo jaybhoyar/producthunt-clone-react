54853b39ae8b9aa82a56d0eafab49acbab88c39b
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-04 16:22:30 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./header.scss";
+
+function Header() {
+	return (
+		<div>
+			<div className="header_contianer">
+				<header className="header">
+					<div className="nav">
+						<div className="logo">
+							<img src="../producthunt_logo.svg" alt="" />
+						</div>
+						<div className="search_box">
+							<span>
+								<svg
+									width="15"
+									height="15"
+									viewBox="0 0 15 15"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M9.383 10.347a5.796 5.796 0 1 1 .965-.964L15 14.036l-.964.964-4.653-4.653zm-3.588-.12a4.432 4.432 0 1 0 0-8.863 4.432 4.432 0 0 0 0 8.863z"
+										fill="#BBB"
+										fill-rule="evenodd"
+									></path>
+								</svg>
+							</span>
+							<input
+								type="text"
+								placeholder="Discover your next favorite thing..."
+							/>
+						</div>
+						<ul>
+							<li>Deals</li>
+							<li>Jobs</li>
+							<li>Makers</li>
+							<li>Radio</li>
+							<li>Ship</li>
+							<li>...</li>
+						</ul>
+					</div>
+					<div className="auth_button">
+						<button className="login_button">Log in</button>
+						<button className="signup_button">Sign up</button>
+					</div>
+				</header>
+			</div>
+			<p className="title"> Today </p>
+		</div>
+	);
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDom from "react-dom";
+import Header from "./Header";
+import Card from "./Card";
+const products = [
+	{
+		name: "Datawaves",
+		description: "Add analytics to anything",
+		tags: ["ANALYTICS", "DEVELOPER TOOLS"],
+		img:
+			"https://ph-files.imgix.net/d58fc63b-7ddf-4e5b-9a29-fb2f1162a05e?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	},
+	{
+		name: "Mailbrew",
+		description:
+			"Automated email digests from Twitter, Reddit, YouTube & more",
+		tags: ["EMAIL", "PRODUCTIVITY"],
+		img:
+			"https://ph-files.imgix.net/f64667ec-0201-4f20-9a03-161ab227a936?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	},
+	{
+		name: "SpotiApp",
+		description: "Export music to Spotify from any musical services",
+		tags: ["IPHONE", "MUSIC"],
+		img:
+			"https://ph-files.imgix.net/331c9402-1670-49ea-9acf-dbffbfc6382d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	},
+	{
+		name: "SelfOne",
+		description: "Free template for PowerPoint, Keynote and Google Slides",
+		tags: ["DESIGN TOOLS", "PRODUCTIVITY"],
+		img:
+			"https://ph-files.imgix.net/3b8fff05-5ac3-4e2b-b902-4606c677d40a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	},
+	{
+		name: "SaaS Landing Page",
+		description:
+			"The best SaaS landing page examples for design inspiration",
+		tags: ["DESIGN TOOLS", "DEVELOPER TOOLS"],
+		img:
+			"https://ph-files.imgix.net/ec8385c2-d317-4cf6-ac1a-e20c8d8f6b89?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	},
+	{
+		name: "Flutter Dating",
+		description: "The live Sunday dating app",
+		tags: ["IPHONE", "DATING"],
+		img:
+			"https://ph-files.imgix.net/2a750d4a-ab47-4b84-a124-3acd044b8ed6?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+	}
+];
+function Cards(props) {
+	return (
+		<div>
+			<Header />
+			{props.data.map(product => (
+				<Card {...product} />
+			))}
+		</div>
+	);
+}
+ReactDom.render(<Cards data={products} />, document.querySelector("#root"));
