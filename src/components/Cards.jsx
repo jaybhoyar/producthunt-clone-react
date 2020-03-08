45a1bfb8dd2ff31af7b26ac959b91ff5f14947c8
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-09 00:36:06 +0530

    minor fix

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,76 +3,76 @@ import Card from "./Card";
 import Sidebar from "./Sidebar";
 import config from "../config.js";
 
-const products = [
-	{
-		name: "SelfOne",
-		description: "Free template for PowerPoint, Keynote and Google Slides",
-		tags: ["DESIGN TOOLS", "PRODUCTIVITY"],
-		vote: 419,
-		img:
-			"https://ph-files.imgix.net/3b8fff05-5ac3-4e2b-b902-4606c677d40a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "Explo",
-		description: "Explore and analyze data without SQL or Excel",
-		tags: ["ANALYTICS", "WEB APP"],
-		vote: 276,
-		img:
-			"https://ph-files.imgix.net/b2d336ea-0c78-430c-8891-cad23d6e316a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "Startup 911",
-		description:
-			"Free list of discounts for startups. All you need for growth",
-		tags: ["DESIGN TOOLS", "PRODUCTIVITY"],
-		vote: 312,
-		img:
-			"https://ph-files.imgix.net/8753162a-ebc7-4237-8d4c-e12e142f2c63?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "ChartMogul",
-		description: "Work with your subscription data like never before.",
-		tags: ["ANALYTICS", "SAAS"],
-		vote: 226,
-		img:
-			"https://ph-files.imgix.net/383c5ede-49db-4a3f-8978-7ba8150907ba?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "Mailbrew",
-		description:
-			"Automated email digests from Twitter, Reddit, YouTube & more",
-		tags: ["EMAIL", "PRODUCTIVITY"],
-		vote: 472,
-		img:
-			"https://ph-files.imgix.net/f64667ec-0201-4f20-9a03-161ab227a936?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "SpotiApp",
-		description: "Export music to Spotify from any musical services",
-		tags: ["IPHONE", "MUSIC"],
-		vote: 354,
-		img:
-			"https://ph-files.imgix.net/331c9402-1670-49ea-9acf-dbffbfc6382d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
+// const products = [
+// 	{
+// 		name: "SelfOne",
+// 		description: "Free template for PowerPoint, Keynote and Google Slides",
+// 		tags: ["DESIGN TOOLS", "PRODUCTIVITY"],
+// 		vote: 419,
+// 		img:
+// 			"https://ph-files.imgix.net/3b8fff05-5ac3-4e2b-b902-4606c677d40a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "Explo",
+// 		description: "Explore and analyze data without SQL or Excel",
+// 		tags: ["ANALYTICS", "WEB APP"],
+// 		vote: 276,
+// 		img:
+// 			"https://ph-files.imgix.net/b2d336ea-0c78-430c-8891-cad23d6e316a?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "Startup 911",
+// 		description:
+// 			"Free list of discounts for startups. All you need for growth",
+// 		tags: ["DESIGN TOOLS", "PRODUCTIVITY"],
+// 		vote: 312,
+// 		img:
+// 			"https://ph-files.imgix.net/8753162a-ebc7-4237-8d4c-e12e142f2c63?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "ChartMogul",
+// 		description: "Work with your subscription data like never before.",
+// 		tags: ["ANALYTICS", "SAAS"],
+// 		vote: 226,
+// 		img:
+// 			"https://ph-files.imgix.net/383c5ede-49db-4a3f-8978-7ba8150907ba?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "Mailbrew",
+// 		description:
+// 			"Automated email digests from Twitter, Reddit, YouTube & more",
+// 		tags: ["EMAIL", "PRODUCTIVITY"],
+// 		vote: 472,
+// 		img:
+// 			"https://ph-files.imgix.net/f64667ec-0201-4f20-9a03-161ab227a936?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "SpotiApp",
+// 		description: "Export music to Spotify from any musical services",
+// 		tags: ["IPHONE", "MUSIC"],
+// 		vote: 354,
+// 		img:
+// 			"https://ph-files.imgix.net/331c9402-1670-49ea-9acf-dbffbfc6382d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
 
-	{
-		name: "SaaS Landing Page",
-		description:
-			"The best SaaS landing page examples for design inspiration",
-		tags: ["DESIGN TOOLS", "DEVELOPER TOOLS"],
-		vote: 627,
-		img:
-			"https://ph-files.imgix.net/ec8385c2-d317-4cf6-ac1a-e20c8d8f6b89?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	},
-	{
-		name: "Flutter Dating",
-		description: "The live Sunday dating app",
-		tags: ["IPHONE", "DATING"],
-		vote: 869,
-		img:
-			"https://ph-files.imgix.net/2a750d4a-ab47-4b84-a124-3acd044b8ed6?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
-	}
-];
+// 	{
+// 		name: "SaaS Landing Page",
+// 		description:
+// 			"The best SaaS landing page examples for design inspiration",
+// 		tags: ["DESIGN TOOLS", "DEVELOPER TOOLS"],
+// 		vote: 627,
+// 		img:
+// 			"https://ph-files.imgix.net/ec8385c2-d317-4cf6-ac1a-e20c8d8f6b89?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	},
+// 	{
+// 		name: "Flutter Dating",
+// 		description: "The live Sunday dating app",
+// 		tags: ["IPHONE", "DATING"],
+// 		vote: 869,
+// 		img:
+// 			"https://ph-files.imgix.net/2a750d4a-ab47-4b84-a124-3acd044b8ed6?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2"
+// 	}
+// ];
 const images = [
 	{
 		img:
@@ -129,6 +129,17 @@ class Cards extends React.Component {
 		this.state = {
 			data: []
 		};
+	}
+	updateCount = vote => {
+		let updatedData = this.state.data.map(product => {
+			if (product.vote === vote) {
+				product.vote = product.vote + 1;
+			}
+			return product;
+		});
+		this.setState({ data: updatedData });
+	};
+	componentDidMount() {
 		fetch("https://api.producthunt.com/v1/posts/", {
 			method: "GET",
 			headers: {
@@ -145,32 +156,6 @@ class Cards extends React.Component {
 			})
 			.catch(err => console.log(err));
 	}
-	updateCount = vote => {
-		let updatedData = this.state.data.map(product => {
-			if (product.vote === vote) {
-				product.vote = product.vote + 1;
-			}
-			return product;
-		});
-		this.setState({ data: updatedData });
-	};
-	// componentDidMount() {
-	// 	fetch("https://api.producthunt.com/v1/posts/", {
-	// 		method: "GET",
-	// 		headers: {
-	// 			Accept: "application/json",
-	// 			"Content-Type": "application/json",
-	// 			Authorization: "Bearer " + config.token,
-	// 			Host: "api.producthunt.com"
-	// 		}
-	// 	})
-	// 		.then(res => res.json())
-	// 		.then(resData => {
-	// 			console.log(resData.posts);
-	// 			this.setState({ data: resData.posts });
-	// 		})
-	// 		.catch(err => console.log(err));
-	// }
 	render() {
 		return (
 			<>
